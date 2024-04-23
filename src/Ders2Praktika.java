37d0f0a9488ba6bd1d0c24a4d4b13ac5a2592f14
Hp <xeyaleqediirova@gmail.com>
2024-04-23 21:58:30 +0400

    initial

---
+++ src/Ders2Praktika.java
@@ -0,0 +1,9 @@
+public class Ders2Praktika {
+    public static void main(String[] args) {
+        int number1 = 9;
+        int number2 = 5 ;
+        int muiltiply = number1*number2;
+
+        System.out.println("netice:"+number1 + "*"+ number2 + "="+ muiltiply);
+    }
+}
---
+++ src/Ders2Praktika3.java
@@ -0,0 +1,9 @@
+public class Ders2Praktika3 {
+    public static void main(String[] args) {
+        int number1 = 9;
+        double number2 = 5 ;
+        double result = number1-number2;
+
+        System.out.println("netice:"+number1 + "-"+ number2 + "="+ result);
+    }
+}
