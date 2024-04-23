05449ad91bb0af7bbbaef7fed079f756dd1e55da
Xeyale Qedirova <xeyaleqediirova@gmail.com>
2024-04-24 03:42:10 +0400

    initial

---
+++ src/VariablesPractice.java
@@ -0,0 +1,20 @@
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int age = 25;
+
+        System.out.println("age : " + age);
+
+        boolean hasJob = false;
+        System.out.println("hasJob : " + hasJob);
+
+        char studentGrade = 'C';
+        System.out.println("studentGrade : " + studentGrade);
+
+        double carPrice = 25000;
+        System.out.println("carPrice : " + carPrice);
+
+        String employeeName = "Məlik";
+        System.out.println("employeeName : " + employeeName);
+
+    }
+}
