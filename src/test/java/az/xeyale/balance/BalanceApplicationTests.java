c979fb26c10b154d123cc14ddd93bd2a2baa2b94
Xeyale Qedirova <xeyaleqediirova@gmail.com>
2024-11-10 12:20:11 +0400

    project created

---
+++ src/test/java/az/xeyale/balance/BalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package az.xeyale.balance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
