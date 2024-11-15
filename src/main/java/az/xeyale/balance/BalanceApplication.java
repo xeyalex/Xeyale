6768d5fbb7f77a176028824ff6396ea3f6212083
Xeyale Qedirova <xeyaleqediirova@gmail.com>
2024-11-15 21:43:10 +0400

    3

---
+++ src/main/java/az/xeyale/balance/BalanceApplication.java
@@ -0,0 +1,13 @@
+package az.xeyale.balance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalanceApplication.class, args);
+    }
+
+}
